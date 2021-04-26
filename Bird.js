05fdd31b1bab6d951e748a3f57e4ae1a158ab048
Sreenasaju <78248390+Sreenasaju@users.noreply.github.com>
2021-04-26 18:57:11 +0530

    Add files via upload

--- Bird.js
+++ Bird.js
@@ -2,11 +2,22 @@ class Bird extends BaseClass {
   constructor(x,y){
     super(x,y,50,50);
     this.image = loadImage("sprites/bird.png");
+    this.smokeImage=loadImage("sprites/smoke.png");
+    this.trajectory = [];
   }
 
   display() {
     //this.body.position.x = mouseX;
     //this.body.position.y = mouseY;
     super.display();
+
+    if(this.body.position.x>220){
+      var pos=[this.body.position.x,this.body.position.y];
+      this.trajectory.push(pos);
+    }  
+  
+    for(var i=0;i< this.trajectory.length;i++){ // trajectory = [[200,130],[230,59],[234,66]]; trajectory[0][1]
+      image(this.smokeImage, this.trajectory[i][0], this.trajectory[i][1]); // image(imagename,x,y,optional_width,optional_height);
+    }
   }
 }
